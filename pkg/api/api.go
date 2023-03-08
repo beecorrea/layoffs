1337d1bf88c18da0149db5d0d5017e2147e8867a
Bianca Corrêa <bee.correa@hotmail.com>
2023-03-08 18:02:37 -0300

    feat: routes for creating and deleting layoff

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,20 +1,33 @@
 package api
 
 import (
+	"context"
 	"log"
 
 	"github.com/beecorrea/layoffs/pkg/api/layoff"
+	"github.com/beecorrea/layoffs/pkg/persistence"
 	"github.com/gofiber/fiber/v2"
 )
 
 func NewLayoffApp() {
+	ctx := context.Background()
+
+	handler := GetComponents(ctx)
+
 	app := fiber.New()
 
-	app.Get("*", func(c *fiber.Ctx) error {
-		return c.SendString("TODO implement api")
-	})
-
-	app.Get("/api/:company?", layoff.ListLayoffs)
-
+	app.Get("/api/layoff/:company?", handler.ListLayoffs)
+	app.Post("/api/layoff", handler.SaveLayoff)
 	log.Fatal(app.Listen(":3000"))
+}
+
+func GetComponents(ctx context.Context) layoff.LayoffHandler {
+	db, err := persistence.Open(ctx)
+	if err != nil {
+		panic(err)
+	}
+	repo := layoff.LayoffRepository{Database: db}
+	service := layoff.NewLayoffService(repo)
+	handler := layoff.NewLayoffHandler(service)
+	return handler
 }
